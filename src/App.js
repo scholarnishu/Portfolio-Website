307ac3b8c24ee817fb790423999227bacbd85e63
Nishant <nishant.sharma@cloudoric.com>
2022-08-14 03:09:54 +0530

    fixing error

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Banner from "./Components/Banner";
 import Header from "./layout/Header";
 import Description from "./Components/Description";
 import Skills from "./Components/Skills";
-import Portfolio from "./Components/Protfolio";
 import Resume from "./Components/Resume";
 import Contact from "./Components/Contact";
 import Footer from "./layout/Footer";
